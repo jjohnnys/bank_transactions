b7b725c39e24474c5887616ab877fc752d25da20
INDRA\jjsilva <jjsilva@minsait.com>
2024-04-15 20:06:29 -0300

    criacao projeto spring boot

---
+++ bank_transactions_api/src/main/java/br/com/jjohnnys/bank_transactions_api/BankTransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jjohnnys.bank_transactions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionsApiApplication.class, args);
+	}
+
+}
---
+++ bank_transactions_api/src/test/java/br/com/jjohnnys/bank_transactions_api/BankTransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jjohnnys.bank_transactions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
